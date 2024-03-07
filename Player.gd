54d46c6adb7bb054a74ea398893705da89e9362b
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-03-08 00:05:23 +0700

    Level 2 Environment

---
+++ Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 300
+export (int) var GRAVITY = 1200
+export (int) var jump_speed = -400
+
+const UP = Vector2(0,-1)
+
+var velocity = Vector2()
+
+func get_input():
+	velocity.x = 0
+	if is_on_floor() and Input.is_action_just_pressed('up'):
+		velocity.y = jump_speed
+		#$Camera2D.current = false -> pas jatoh
+	if Input.is_action_pressed('right'):
+		velocity.x += speed
+	if Input.is_action_pressed('left'):
+		velocity.x -= speed
+
+func _physics_process(delta):
+	$AnimatedSprite.play("default")
+	velocity.y += delta * GRAVITY
+	get_input()
+	velocity = move_and_slide(velocity, UP)
